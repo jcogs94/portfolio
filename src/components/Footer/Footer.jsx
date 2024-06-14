985c1132ea38add44f64cd43d5bbb6149cc06e38
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-13 23:14:32 -0400

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,18 @@ import './Footer.css'
 
 const Footer = () => {
     return <>
-        {/*  */}
+        <footer>
+            <div id="contact-me">
+                <h4>Contact Me</h4>
+                <a href="mailto:jcogs1994@gmail.com">jcogs1994@gmail.com</a>
+                <a href="tel:+19044443735">(904) 444-3735</a>
+            </div>
+            <div id="socials">
+                <h4>Socials</h4>
+                <a href="http://www.linkedin.com/in/jonathan-cogswell-2b8588173">LinkedIn</a>
+                <a href="https://github.com/jcogs94">GitHub</a>
+            </div>
+        </footer>
     </>
 }
 
