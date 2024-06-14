c194a8518545e51481a9a54c8eabb6da765864c6
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-13 22:53:12 -0400

    file structure and placeholder nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Main from './components/Main/Main.jsx';
+import Footer from './components/Footer/Footer.jsx';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <Navbar />
+    <Main />
+    <Footer />
+  </>
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return <>
+        <nav>
+            <a>Home</a>
+            <a>About Me</a>
+            <a>Projects</a>
+        </nav>
+    </>
+}
+
+export default Navbar
