bccdb68f266683b024f2287161b7e3b983024728
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-30 02:36:09 -0400

    dates added to projects

--- src/components/Main/MyWork/ProjectCard/ProjectCard.jsx
+++ src/components/Main/MyWork/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,9 @@
 import './ProjectCard.css'
 
-const ProjectCard = ({ img, name, description, liveLink, githubLink }) => {
+const ProjectCard = ({ img, name, date, description, liveLink, githubLink }) => {
     return <>
         <div className="project-card">
-            <h3>{name}</h3>
+            <h3>{name} - {date}</h3>
             <img src={img} alt={name} />
             <p><em>{description}</em></p>
             <div className='project-card-link-container'>
--- src/components/Main/MyWork/projectsData.js
+++ src/components/Main/MyWork/projectsData.js
@@ -8,6 +8,7 @@ const projectsData = [
     {
         img: utiliSuite,
         name: "UtiliSuite",
+        date: "June 2024",
         description: "A suite of utility tools all in one application, using a Spring Boot back end and React front end.",
         liveLink: "http://projects.joncogswell.com:8086/",
         githubLink: "https://github.com/jcogs94/utiliSuite.git"
@@ -15,6 +16,7 @@ const projectsData = [
     {
         img: instrumentManagement,
         name: "Instrument Rental Management",
+        date: "June 2024",
         description: "An organization and management tool for tracking instrument rentals. I worked as the Lead Front-End Developer for this team project.",
         liveLink: "http://projects.joncogswell.com:8084/",
         githubLink: "https://github.com/jcogs94/unit4-front-end.git"
@@ -22,6 +24,7 @@ const projectsData = [
     {
         img: todo,
         name: "ToDo or Not ToDo",
+        date: "May 2024",
         description: "A full stack MERN application for managing ToDo tasks. For this group project I worked as the Front-End Manager and Lead Developer.",
         liveLink: "http://projects.joncogswell.com:8081/",
         githubLink: "https://github.com/jcogs94/todo-or-not-todo-frontend.git"
@@ -29,6 +32,7 @@ const projectsData = [
     {
         img: cruddyCash,
         name: "CRUDdy Cash",
+        date: "May 2024",
         description: "Personal budgeting application with full CRUD capabilities, utilizing user authentication and mobile-focused development.",
         liveLink: "http://projects.joncogswell.com:8085/",
         githubLink: "https://github.com/jcogs94/cruddy-cash.git"
@@ -36,6 +40,7 @@ const projectsData = [
     {
         img: qwixx,
         name: "Qwixx",
+        date: "April 2024",
         description: "Based on the dice game produced by GameWright, this app allows users to play the game without pen and paper.",
         liveLink: "https://qwixx.joncogswell.com/",
         githubLink: "https://github.com/jcogs94/qwixx.git"
