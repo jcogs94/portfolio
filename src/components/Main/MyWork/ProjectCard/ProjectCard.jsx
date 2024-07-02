819e7b6c1877f72f047761b52d5df1d9a777191c
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-02 06:20:25 -0400

    skills added to dom

--- src/components/Main/MyWork/ProjectCard/ProjectCard.jsx
+++ src/components/Main/MyWork/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,20 @@
 import './ProjectCard.css'
 
-const ProjectCard = ({ img, name, date, description, liveLink, githubLink }) => {
+const ProjectCard = ({ img, name, date, description, skills, liveLink, githubLink }) => {
     return <>
         <div className="project-card">
             <h3>{name} - {date}</h3>
             <img src={img} alt={name} />
             <p><em>{description}</em></p>
+            <p><b>Skills Used: </b>{
+                skills.map( (skill, index) => {
+                    if (index + 1 === skills.length) {
+                        return skill
+                    } else {
+                        return skill + ", "
+                    }
+                })
+            }</p>
             <div className='project-card-link-container'>
                 <a href={liveLink}><button className='project-link'>Live Site</button></a>
                 <a href={githubLink}><button className='project-link'>GitHub</button></a>
